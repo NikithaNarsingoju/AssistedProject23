9e4d1fa845303349e984b8bd5dd049b68838314f
Nikitha <nikithanarsingoju1@gmail.com>
2023-10-27 15:26:47 +0530

    Add project files.

---
+++ SubjectTeacher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace AssistedProject23
+{
+    public class SubjectTeacher : Teacher 
+    {
+        private Subject whichSubject;
+
+        public Subject WhichSubject
+        {
+            get { return whichSubject; }
+            set { whichSubject = value; }
+        }
+    }
+}
+  
